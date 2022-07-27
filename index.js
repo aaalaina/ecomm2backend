e00505cc2ef312076aed0ae3c80a76c0354a81a8
Alaina H <97alaina@gmail.com>
2022-07-26 20:00:46 -0400

    first backend commit

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql2');
+const bodyParser = require('body-parser');
+const dotenv = require('dotenv').config();
+const port = 3000;
+app.use(express.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+
+
+const pool  = mysql.createPool({
+  connectionLimit : 8,
+  host            : process.env.HOST,
+  user            : process.env.USERNAME,
+  port            : process.env.DB_PORT,
+  password        : process.env.PASSWORD,
+  database        : process.env.DATABASE
+});
+//req means REQUIRE....not request...... crazy
+app.get("/", (req,res) => {
+      pool.query(`SELECT * FROM plushie_products`, (err, results) => {
+        if (err) {
+          console.log(err)
+        } else {res.send(results)}
+      })
+    });
+
+
+
+
+app.listen(3000, () =>
+console.log("Running server on port 3000!")
+)
